b2c0fbbc1e92dabea19062142b787e1e0ba5d1de
shiva-verma <jztshiva@gmail.com>
2025-09-02 19:33:38 +0530

    medium

---
+++ SECTION-AD/Lec-13/script.js
@@ -0,0 +1,10 @@
+function sum (){
+    let a;
+    var b = {};
+    const c = null;
+    console.log(typeof(b))
+    console.log(typeof(a))
+    console.log(typeof(c));
+}
+
+console.log(typeof(sum))

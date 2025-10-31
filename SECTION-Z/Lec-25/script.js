d5197b5a6f097ae1ed316346a926645da93fc009
shiva-verma <jztshiva@gmail.com>
2025-10-31 11:32:36 +0530

    addtodo

---
+++ SECTION-Z/Lec-25/script.js
@@ -0,0 +1,34 @@
+let inputBox = document.getElementById("inp");
+let addBtn = document.getElementById("btn");
+let list = document.getElementById("list");
+
+
+function addTodoTask(){
+   let inputValue = inputBox.value
+   
+   const li = document.createElement("li");
+   li.innerText = inputValue;
+   li.classList.add("list-item")
+
+   const delBtn = document.createElement("button");
+   delBtn.innerText = "Delete";
+   delBtn.classList.add("delBtn")
+
+   const editBtn = document.createElement("button");
+   editBtn.innerText = "Edit";
+   editBtn.classList.add("editBtn")
+
+   function removeItem(){
+        list.removeChild(li);
+   }
+
+   delBtn.addEventListener("click", removeItem);
+
+   li.appendChild(delBtn);
+   li.appendChild(editBtn);
+   list.appendChild(li);
+
+   inputBox.value = ""
+}
+
+addBtn.addEventListener("click", addTodoTask)

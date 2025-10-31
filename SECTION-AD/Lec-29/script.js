acf4c6b68ba71d326313c6bf007396808a59e75d
shiva-verma <jztshiva@gmail.com>
2025-10-31 12:52:17 +0530

    weather app

--- SECTION-AD/Lec-29/script.js
+++ SECTION-AD/Lec-29/script.js
@@ -1,10 +1,21 @@
 let inputBox = document.getElementById("cityInput");
 let searchBtn = document.getElementById("searchBtn");
 
-function getWeather(){
-    let city = inputBox.value;
+async function getWeather(e){
+    e.preventDefault();
+    let city_name = inputBox.value;
+    let API_key = ""
 
-    let weatherData = fetch(`https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`)
+    let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=1&appid=${API_key}`)
+    let geoData = await response.json();
+
+    const {lat, lon} = geoData[0]
+
+    //getWeather
+    let response2 = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_key}`)
+    let getW = await response2.json();
+
+    console.log(getW)
 }
 
 searchBtn.addEventListener("click", getWeather);

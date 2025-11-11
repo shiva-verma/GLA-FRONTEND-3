4aa4a19b7f05cbdb8316145b601dd0f3deb0b64f
shiva-verma <jztshiva@gmail.com>
2025-11-11 14:43:45 +0530

    sec-N dom

---
+++ SECTION-AB/Lec-32/reacttute/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from './Components/Card'
+import Footer from './Components/Footer'
+import Navbar from './Components/Navbar'
+
+function App(){
+  return (
+    <>
+    <Navbar/>
+    <Card/>
+    <Card/>
+    <Footer/>
+    </>
+  )
+}
+
+
+export default App
---
+++ SECTION-AF/Lec-27/reacttute/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./Components/Navbar"
+
+function App() {
+  return (
+      <>
+       <Navbar/>
+        <Navbar/>
+         <Navbar/>
+          <Navbar/>
+           <Navbar/>
+            <Navbar/>
+             <Navbar/>
+              <Navbar/>
+      </>
+  )
+}
+
+export default App
